3c0cfdbd95bb096fa76bafa89734bfed0af9a6df
= <=>
2021-01-11 22:01:35 +0100

    stage new files for Issue #102, Issue #105

---
+++ app/Http/Controllers/EquipmentWarrantyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EquipmentWarranty;
+use Illuminate\Http\Request;
+
+class EquipmentWarrantyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EquipmentWarranty  $equipmentWarranty
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EquipmentWarranty $equipmentWarranty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EquipmentWarranty  $equipmentWarranty
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EquipmentWarranty $equipmentWarranty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EquipmentWarranty  $equipmentWarranty
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EquipmentWarranty $equipmentWarranty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EquipmentWarranty  $equipmentWarranty
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EquipmentWarranty $equipmentWarranty)
+    {
+        //
+    }
+}
---
+++ database/factories/EquipmentWarrantyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\EquipmentWarranty;
+use Faker\Generator as Faker;
+
+$factory->define(EquipmentWarranty::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
