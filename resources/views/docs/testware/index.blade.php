a2e65aacdbb28585d22f6567873bc1c4fc6e55e3
Nasstassja Braunsdorf <nassti@bitpack.io>
2021-06-30 12:27:14 +0200

    Update typo Objekte index.blade.php

--- resources/views/docs/testware/index.blade.php
+++ resources/views/docs/testware/index.blade.php
@@ -47,13 +47,13 @@
                                  class="img-fluid"
                             >
                             <p>Das <code>Produkt</code> definiert die Eigenschaften des Gerätes. Angefangen von der Kategorie, der Artikelnummer bis zur Einteilung der anzuwendenden Prüfung oder mehreren Prüfungen. Das <code>Produkt</code> ist eine Vorlage, aus der Geräte erstellt werden können.</p>
-                            <p>Ändert sich beispielsweise ein Prüfvorschrift des Produktes werden automatisch aller Geräte, die aus diesem Produkt erstellt wurden, aktualisiert.</p>
+                            <p>Ändert sich beispielsweise ein Prüfvorschrift des Produktes werden automatisch alle Geräte, die aus diesem Produkt erstellt wurden, aktualisiert.</p>
                             <p>Der <code>Stellplatz</code> definiert den Ort an dem das Gerät sich befindet. Bei mobilen Geräten ist der Stellplatz der Ort, an dem das Gerät beim Nichtbetreiben lagert. Dies kann zum Beispiel die Ladestation sein.</p>
                             <p>Für Geräte können auch einen Raum oder ein Gebäude als Stellplatz definiert werden. Ganz den Anfoderungen des Gerätes entsprechend.</p>
                             <p>Näheres zu den Objekten finden Sie in den entsprechenden Sektionen.</p>
                             <h2 class="h4">Produkte</h2>
                             <p><code>Produkte</code> sind Vorlagen, aus denen Geräte erstellt werden können. Sie beinhalten alle gemeinsamen Werte, wie Hersteller, Artikelnummer oder die Bedienungsanleitung.</p>
-                            <p><code>Produkte</code> können <code>Verordnungen</code> und dadurch bestimmten Anforderungen unterliegen. Dadurch ergeben sich Prüf- und/oder Wartungsvorgänge. Diese Vorgänge zu überwachen ist die Hauptaufgabe der testWare</p>
+                            <p><code>Produkte</code> können <code>Verordnungen</code> und dadurch bestimmten Anforderungen unterliegen. Dadurch ergeben sich Prüf- und/oder Wartungsvorgänge. Diese Vorgänge zu überwachen ist die Hauptaufgabe der testWare.</p>
                             <p><span class="lead">Beispiel:</span></p>
                             <p>Elektrogeräte unterliegen der VDE Geräteprüfung VDE 0100-600. Nach dieser Verordnung sind Elektrogeräte in 4 Klassen einzuteilen:</p>
                             <ul>
