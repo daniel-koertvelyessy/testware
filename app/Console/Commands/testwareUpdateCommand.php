82936ddcb24cc733ef3e1ce978698781363382bb
Daniel Körtvélyessy <daniel.koertvelyessy@bitpack.io>
2021-09-06 14:00:01 +0200

    Implement TestReportFormat
    Add new table/model for test report id format
    Add command testare:dbupdate

---
+++ app/Console/Commands/testwareUpdateCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class testwareUpdateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'testware:dbupdate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update testware database to new schema';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        parent::__construct();
+
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Start working ... ');
+        $output = '';
+        \Artisan::call('migrate',[],$output);
+        $this->info(\Artisan::output());
+
+        return 0;
+    }
+}
---
+++ database/migrations/2021_09_06_132920_create_test_report_formats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestReportFormatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_report_formats', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedTinyInteger('digits')->default(6);
+            $table->string('prefix',10)->nullable();
+            $table->string('postfix',10)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_report_formats');
+    }
+}
