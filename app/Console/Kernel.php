294196a2149a0e9188c532d765b2df1395c1f836
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2021-10-02 22:17:46 +0200

    Update Kernel.php
    set schedule to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 //use App\Jobs\CheckEquipmentTestDueDates;
+use App\Jobs\CheckEquipmentTestDueDates;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->job(new CheckEquipmentTestDueDates())->everyFourMinutes();
+        $schedule->job(new CheckEquipmentTestDueDates())->daily();
     }
 
     /**
