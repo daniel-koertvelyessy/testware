6929981d1c5b0e47f86643ace254b6a475a6537f
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2024-12-19 10:58:07 +0100

    bump 1.81.16
    - update npm packages
    - fix interval_type_selector blade component

--- resources/views/components/lists/intervalTypeSelector.blade.php
+++ resources/views/components/lists/intervalTypeSelector.blade.php
@@ -1,7 +1,13 @@
+@props([
+    'name' => $name ?? 'control_interval_id_'.\Illuminate\Support\Str::random(3),
+    'id' => $id ?? 0,
+    'selected' => 0,
+    'intervalTypeList' => []
+])
 <section>
     <label for="control_interval_id_{{ $id }}">{{ __('Intervall') }}</label>
     <select class="custom-select"
-            id="{{ $name . $id }}"
+            id="control_interval_id_{{ $id }}"
             name="{{ $name }}"
     >
         <option {{ $selected === null ? "selected" : "" }} value="0">{{ __('bitte auswählen') }}</option>
