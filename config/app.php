526a338a12af4f519d6abdda04d46732085449b4
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2026-04-21 11:35:38 +0200

    bump v1.81.32
    update packages
    remove 'IdeHelperServiceProvider' from config/app.php

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Elibyy\TCPDF\ServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
     ],
 
